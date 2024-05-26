0e7c20dcf86354378f5be8b62d384439e4dfe50c
dev_margooo <luckinamargo@gmail.com>
2024-05-26 13:23:38 +0300

    Добавлен скелет проекта

---
+++ src/main/java/ru/gazprombank/lukinamiddleservice/LukinaMiddleServiceApplication.java
@@ -0,0 +1,13 @@
+package ru.gazprombank.lukinamiddleservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LukinaMiddleServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LukinaMiddleServiceApplication.class, args);
+	}
+
+}

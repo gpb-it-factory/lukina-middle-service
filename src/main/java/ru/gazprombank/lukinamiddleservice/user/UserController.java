1417f422e902c36dbbe7c34a2de69897b2653a4f
dev_margooo <luckinamargo@gmail.com>
2024-06-02 01:23:33 +0300

    Добавлен эндпойнт users

--- src/main/java/ru/gazprombank/lukinamiddleservice/user/UserController.java
+++ src/main/java/ru/gazprombank/lukinamiddleservice/user/UserController.java
@@ -1,16 +1,29 @@
 package ru.gazprombank.lukinamiddleservice.user;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    private String host;
+    private RestTemplate restTemplate;
+
+    public UserController(@Value("${backend.host}") String host) {
+        super();
+        this.host = host;
+        this.restTemplate = new RestTemplate();
+    }
     @PostMapping
     public String registerUser() {
-        return "OK";
+        String url = this.host + "/users";
+        String response = restTemplate.postForObject(url, null, String.class);
+        System.out.println("Response from register: " + response);
+        return response;
     }
     @GetMapping("/test")
     public String test() {
